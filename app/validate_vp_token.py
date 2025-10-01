d8a6abf7419b5389477bffe6b4707fa7cd4192fa
Tomás de Sá <61152929+tomasdesa@users.noreply.github.com>
2025-10-01 12:09:25 +0100

    newPID login

--- app/validate_vp_token.py
+++ app/validate_vp_token.py
@@ -91,7 +91,7 @@ def validate_vp_token(response_json):
 
     else:
 
-        mdoc = response_json["vp_token"][0]
+        mdoc = response_json["vp_token"]["query_0"]
         
         mdoc_ver = None
 
