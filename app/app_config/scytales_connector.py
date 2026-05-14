12bd45718493a13f2540e34abf9d377d335d19ff
Rolando2000 <61158161+Rolando2000@users.noreply.github.com>
2026-05-14 11:36:03 +0100

    scytales connector

---
+++ app/app_config/scytales_connector.py
@@ -0,0 +1,41 @@
+# coding: latin-1
+###############################################################################
+# Copyright (c) 2026 European Commission
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+###############################################################################
+"""
+This scytales_connector.py contains configuration data for the scytales oauth service. 
+
+NOTE: You should only change it if you understand what you're doing.
+"""
+
+import logging
+from logging.handlers import TimedRotatingFileHandler
+import os
+import random
+from flask import  session
+import logging
+from logging.handlers import TimedRotatingFileHandler
+
+class scytales:
+    
+    name='scytales',
+    client_id=os.environ.get('WALLET_CONNECTOR_CLIENT_ID'),
+    client_secret=os.environ.get('WALLET_CONNECTOR_CLIENT_SECRET'),
+    server_metadata_url='https://idp.connector.scytales/.well-known/openid-configuration',
+    client_kwargs={
+        'scope': 'openid scope:pid',  # Request PID / Mobile Driver's License
+        'code_challenge_method': 'S256'  # Enable PKCE
+    }
