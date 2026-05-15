4eda66f8fcf9dd70a05fbd9ad020017ec0306275
Rolando2000 <61158161+Rolando2000@users.noreply.github.com>
2026-05-15 11:10:43 +0100

    Update RPR_routes.py

--- app/RPR_routes.py
+++ app/RPR_routes.py
@@ -205,7 +205,7 @@ responses:
     }
     if request.args.get("type") and request.args.get("type") == "scytales_connector":
 
-        redirect_uri = url_for('RPR.callback', _external=True)
+        redirect_uri = "https://registry.serviceproviders.eudiw.dev/callback"
         print("redirect_uri = ", redirect_uri)
         client = oauth.create_client(scytales.name)
 
